bdcf70145739cababda3514704131084dd42e80a
Raphael Michel <mail@raphaelmichel.de>
2021-03-24 11:32:54 +0100

    Add more payment methods

--- pretix_payone/payment.py
+++ pretix_payone/payment.py
@@ -1,9 +1,10 @@
 import hashlib
 import json
 import logging
-import requests
 import urllib.parse
 from collections import OrderedDict
+
+import requests
 from django import forms
 from django.conf import settings
 from django.contrib import messages
@@ -11,17 +12,18 @@ from django.core import signing
 from django.http import HttpRequest
 from django.template.loader import get_template
 from django.utils.translation import get_language, gettext_lazy as _
+from pretix_payone.models import ReferencedPayoneObject
+from requests import HTTPError
+
 from pretix.base.decimal import round_decimal
 from pretix.base.forms import SecretKeySettingsField
 from pretix.base.forms.questions import guess_country
 from pretix.base.models import Event, InvoiceAddress, OrderPayment, OrderRefund
 from pretix.base.payment import BasePaymentProvider, PaymentException
 from pretix.base.settings import SettingsSandbox
+from pretix.helpers.countries import CachedCountries
 from pretix.helpers.urls import build_absolute_uri as build_global_uri
 from pretix.multidomain.urlreverse import build_absolute_uri
-from requests import HTTPError
-
-from pretix_payone.models import ReferencedPayoneObject
 
 logger = logging.getLogger(__name__)
 
@@ -35,14 +37,6 @@ class PayoneSettingsHolder(BasePaymentProvider):
     def __init__(self, event: Event):
         super().__init__(event)
         self.settings = SettingsSandbox("payment", "payone", event)
-
-    @property
-    def test_mode_message(self):
-        if self.event.testmode:
-            return _(
-                "The PAYONE plugin is operating in test mode. No money will actually be transferred."
-            )
-        return None
 
     @property
     def settings_form_fields(self):
@@ -82,6 +76,17 @@ class PayoneSettingsHolder(BasePaymentProvider):
             ("sepadebit", _("SEPA direct debit")),
             ("paypal", _("PayPal")),
             ("eps", _("eps")),
+            ("sofort", _("SOFORT")),
+            ("ideal", _("iDEAL")),
+            ("przelewy24", _("Przelewy24")),
+            ("multibanco", _("Multibanco")),
+            ("bancontact", _("Bancontact")),
+            ("vkp", _("Verkkopankki")),
+            ("mybank", _("MyBank")),
+            ("alipay", _("Alipay")),
+            ("paydirekt", _("paydirekt")),
+            ("paysafecard", _("paysafecard")),
+            ("qiwi", _("Qiwi")),
             # more: https://docs.payone.com/display/public/PLATFORM/General+information
         ]
         d = OrderedDict(
@@ -131,6 +136,14 @@ class PayoneMethod(BasePaymentProvider):
         return "payone_{}".format(self.method)
 
     @property
+    def test_mode_message(self):
+        if self.event.testmode:
+            return _(
+                "The PAYONE plugin is operating in test mode. No money will actually be transferred."
+            )
+        return None
+
+    @property
     def is_enabled(self) -> bool:
         return self.settings.get("_enabled", as_type=bool) and self.settings.get(
             "method_{}".format(self.method), as_type=bool
@@ -150,7 +163,11 @@ class PayoneMethod(BasePaymentProvider):
 
     def payment_form_render(self, request) -> str:
         template = get_template("pretix_payone/checkout_payment_form.html")
-        ctx = {"request": request, "event": self.event, "settings": self.settings}
+        if self.payment_form_fields:
+            form = self.payment_form(request)
+        else:
+            form = None
+        ctx = {"request": request, "event": self.event, "settings": self.settings, "form": form}
         return template.render(ctx)
 
     def checkout_confirm_render(self, request) -> str:
@@ -387,16 +404,16 @@ class PayoneMethod(BasePaymentProvider):
             if ia.city:
                 d["city"] = ia.city[:50]
             if ia.state and ia.country in (
-                "US",
-                "CA",
-                "CN",
-                "JP",
-                "MX",
-                "BR",
-                "AR",
-                "ID",
-                "TH",
-                "IN",
+                    "US",
+                    "CA",
+                    "CN",
+                    "JP",
+                    "MX",
+                    "BR",
+                    "AR",
+                    "ID",
+                    "TH",
+                    "IN",
             ):
                 d["state"] = ia.state
 
@@ -463,9 +480,9 @@ class PayoneMethod(BasePaymentProvider):
         if request.session.get("iframe_session", False):
             signer = signing.Signer(salt="safe-redirect")
             return (
-                build_absolute_uri(request.event, "plugins:pretix_payone:redirect")
-                + "?url="
-                + urllib.parse.quote(signer.sign(url))
+                    build_absolute_uri(request.event, "plugins:pretix_payone:redirect")
+                    + "?url="
+                    + urllib.parse.quote(signer.sign(url))
             )
         else:
             return str(url)
@@ -491,10 +508,10 @@ class PayoneCC(PayoneMethod):
         if ppan:
             request.session["payment_payone_pseudocardpan"] = ppan
             for f in (
-                "truncatedcardpan",
-                "cardtypeResponse",
-                "cardexpiredateResponse",
-                "cardholder",
+                    "truncatedcardpan",
+                    "cardtypeResponse",
+                    "cardexpiredateResponse",
+                    "cardholder",
             ):
                 request.session[f"payment_payone_{f}"] = request.POST.get(
                     f"payone_{f}", ""
@@ -552,14 +569,13 @@ class PayoneCC(PayoneMethod):
         return template.render(ctx)
 
 
-class PayoneGiropay(PayoneMethod):
+class PayoneGiropay(PayoneMethod):  # untested
     method = "giropay"
     verbose_name = _("giropay via PAYONE")
     public_name = _("giropay")
     clearingtype = "sb"
     onlinebanktransfertype = "GPY"
     onlinebanktransfer_countries = ("DE",)
-    # todo: ask for country for others like this
 
 
 class PayoneEPS(PayoneMethod):
@@ -569,15 +585,190 @@ class PayoneEPS(PayoneMethod):
     clearingtype = "sb"
     onlinebanktransfertype = "EPS"
     onlinebanktransfer_countries = ("AT",)
+    banks = (
+        ('ARZ_OAB', 'Apothekerbank'),
+        ('ARZ_BAF', 'Ärztebank'),
+        ('BA_AUS', 'Bank Austria'),
+        ('ARZ_BCS', 'Bankhaus Carl Spängler & Co.AG'),
+        ('EPS_SCHEL', 'Bankhaus Schelhammer & Schattera AG'),
+        ('BAWAG_PSK', 'BAWAG P.S.K.AG'),
+        ('BAWAG_ESY', 'Easybank AG'),
+        ('SPARDAT_EBS', 'Erste Bank und Sparkassen'),
+        ('ARZ_HAA', 'Hypo Alpe-Adria-Bank International AG'),
+        ('ARZ_VLH', 'Hypo Landesbank Vorarlberg'),
+        ('HRAC_OOS', 'HYPO Oberösterreich, Salzburg, Steiermark'),
+        ('ARZ_HTB', 'Hypo Tirol Bank AG'),
+        ('EPS_OBAG', 'Oberbank AG'),
+        ('RAC_RAC', 'Raiffeisen Bankengruppe Österreich'),
+        ('EPS_SCHOELLER', 'Schoellerbank AG'),
+        ('ARZ_OVB', 'Volksbank Gruppe'),
+        ('EPS_VRBB', 'VR-Bank Braunau'),
+        ('EPS_AAB', 'Austrian Anadi Bank AG'),
+        ('EPS_BKS', 'BKS Bank AG'),
+        ('EPS_BKB', 'Brüll Kallmus Bank AG'),
+        ('EPS_VLB', 'BTV VIER LÄNDER BANK'),
+        ('EPS_CBGG', 'Capital Bank Grawe Gruppe AG'),
+        ('EPS_DB', 'Dolomitenbank'),
+        ('EPS_NOEGB', 'HYPO NOE Gruppe Bank AG'),
+        ('EPS_NOELB', 'HYPO NOE Landesbank AG'),
+        ('EPS_HBL', 'HYPO-BANK BURGENLAND Aktiengesellschaft'),
+        ('EPS_MFB', 'Marchfelder Bank'),
+        ('EPS_SPDBW', 'Sparda Bank Wien'),
+        ('EPS_SPDBA', 'SPARDA-BANK AUSTRIA'),
+        ('EPS_VKB', 'Volkskreditbank AG'),
+    )
+
+    def _get_payment_params(self, request, payment):
+        p = super()._get_payment_params(request, payment)
+        p['bankgrouptype'] = request.session['payment_payone_eps_bank']
+        return p
+
+    def checkout_prepare(self, request, cart):
+        form = self.payment_form(request)
+        if form.is_valid():
+            request.session['payment_payone_eps_bank'] = form.cleaned_data['bank']
+            return super().checkout_prepare(request, cart)
+        return False
+
+    def payment_is_valid_session(self, request):
+        return (
+                super().payment_is_valid_session(request) and
+                request.session.get('payment_payone_eps_bank', '') != ''
+        )
+
+    @property
+    def payment_form_fields(self):
+        return OrderedDict([
+            ('bank', forms.ChoiceField(label=_('Bank'), choices=self.banks)),
+        ])
 
 
-class PayoneSEPADebit(PayoneMethod):
-    method = "sepadebit"
-    verbose_name = _("SEPA direct debit via PAYONE")
-    public_name = _("SEPA direct debit")
-    clearingtype = "elv"
-    invoice_address_mandatory = True
-    # todo: ask for account details
+class PayoneIdeal(PayoneMethod):
+    method = "ideal"
+    verbose_name = _("iDEAL via PAYONE")
+    public_name = _("iDEAL")
+    clearingtype = "sb"
+    onlinebanktransfertype = "IDL"
+    onlinebanktransfer_countries = ("NL",)
+    banks = (
+        ('ABN_AMRO_BANK', 'ABN Amro'),
+        ('BUNQ_BANK', 'Bunq'),
+        ('RABOBANK', 'Rabobank'),
+        ('ASN_BANK', 'ASN Bank'),
+        ('SNS_BANK', 'SNS Bank'),
+        ('TRIODOS_BANK', 'Triodos Bank'),
+        ('SNS_REGIO_BANK', 'Regio Bank'),
+        ('ING_BANK', 'ING Bank'),
+        ('KNAB_BANK', 'Knab'),
+        ('VAN_LANSCHOT_BANKIERS', 'van Lanschot'),
+        ('HANDELSBANKEN', 'Handelsbanken'),
+        ('MONEYOU', 'Moneyou'),
+    )
+
+    def _get_payment_params(self, request, payment):
+        p = super()._get_payment_params(request, payment)
+        p['bankgrouptype'] = request.session['payment_payone_ideal_bank']
+        return p
+
+    def checkout_prepare(self, request, cart):
+        form = self.payment_form(request)
+        if form.is_valid():
+            request.session['payment_payone_ideal_bank'] = form.cleaned_data['bank']
+            return super().checkout_prepare(request, cart)
+        return False
+
+    def payment_is_valid_session(self, request):
+        return (
+                super().payment_is_valid_session(request) and
+                request.session.get('payment_payone_ideal_bank', '') != ''
+        )
+
+    @property
+    def payment_form_fields(self):
+        return OrderedDict([
+            ('bank', forms.ChoiceField(label=_('Bank'), choices=self.banks)),
+        ])
+
+
+class PayoneSofort(PayoneMethod):
+    method = "sofort"
+    verbose_name = _("SOFORT via PAYONE")
+    public_name = _("SOFORT")
+    clearingtype = "sb"
+    onlinebanktransfertype = "PNT"
+    onlinebanktransfer_countries = ("DE", "AT", "CH", "NL", "PL", "BE",)
+
+    def _get_payment_params(self, request, payment):
+        p = super()._get_payment_params(request, payment)
+        p['bankcountry'] = request.session['payment_payone_sofort_bankcountry']
+        return p
+
+    def checkout_prepare(self, request, cart):
+        form = self.payment_form(request)
+        if form.is_valid():
+            request.session['payment_payone_sofort_bankcountry'] = form.cleaned_data['bankcountry']
+            return super().checkout_prepare(request, cart)
+        return False
+
+    def payment_is_valid_session(self, request):
+        return (
+            super().payment_is_valid_session(request) and
+            request.session.get('payment_payone_sofort_bankcountry', '') != ''
+        )
+
+    @property
+    def payment_form_fields(self):
+        countries = CachedCountries()
+        return OrderedDict([
+            ('bankcountry', forms.ChoiceField(label=_('Bank country'), choices=(
+                (c, countries.name(c)) for c in self.onlinebanktransfer_countries
+            ))),
+        ])
+
+
+class PayonePrzelewy24(PayoneMethod):
+    method = "przelewy24"
+    verbose_name = _("Przelewy24 via PAYONE")
+    public_name = _("Przelewy24")
+    clearingtype = "sb"
+    onlinebanktransfertype = "P24"
+    onlinebanktransfer_countries = ("PL",)
+
+
+class PayoneMultibanco(PayoneMethod):
+    method = "multibanco"
+    verbose_name = _("Multibanco via PAYONE")
+    public_name = _("Multibanco")
+    clearingtype = "sb"
+    onlinebanktransfertype = "MBC"
+    onlinebanktransfer_countries = ("PT",)
+
+
+class PayoneBancontact(PayoneMethod):
+    method = "bancontact"
+    verbose_name = _("Bancontact via PAYONE")
+    public_name = _("Bancontact")
+    clearingtype = "sb"
+    onlinebanktransfertype = "BCT"
+    onlinebanktransfer_countries = ("BE",)
+
+
+class PayoneVerkkopankki(PayoneMethod):
+    method = "vkp"
+    verbose_name = _("Verkkopankki via PAYONE")
+    public_name = _("Verkkopankki")
+    clearingtype = "sb"
+    onlinebanktransfertype = "VKP"
+    onlinebanktransfer_countries = ("FI",)
+
+
+class PayoneMyBank(PayoneMethod):
+    method = "mybank"
+    verbose_name = _("MyBank via PAYONE")
+    public_name = _("MyBank")
+    clearingtype = "sb"
+    onlinebanktransfertype = "MYB"
+    onlinebanktransfer_countries = ("IT",)
 
 
 class PayonePayPal(PayoneMethod):
@@ -588,4 +779,66 @@ class PayonePayPal(PayoneMethod):
     wallettype = "PPE"
 
 
-# todo: sofort, eps, postfinance e-fiance, postfinance card, ideal, przelewy24, bancontact, masterpass, amazon, alipay, paydirekt
+class PayoneAlipay(PayoneMethod):
+    method = "alipay"
+    verbose_name = _("Alipay via PAYONE")
+    public_name = _("Alipay")
+    clearingtype = "wlt"
+    wallettype = "ALP"
+
+    def payment_partial_refund_supported(self, payment: OrderPayment) -> bool:
+        return False
+
+
+class PayonePaydirekt(PayoneMethod):
+    method = "paydirekt"
+    verbose_name = _("paydirekt via PAYONE")
+    public_name = _("paydirekt")
+    clearingtype = "wlt"
+    wallettype = "PDT"
+
+    def _get_payment_params(self, request, payment):
+        d = super()._get_payment_params(request, payment)
+        d['add_paydata[shopping_cart_type]'] = 'DIGITAL'
+        d['email'] = payment.order.email
+        # TODO some kind of "workorder" required?
+        return d
+
+
+class PayonePaysafecard(PayoneMethod):
+    method = "paysafecard"
+    verbose_name = _("paysafeard via PAYONE")
+    public_name = _("paysafecard")
+    clearingtype = "wlt"
+    wallettype = "PSC"
+
+
+class PayoneQiwi(PayoneMethod):
+    method = "qiwi"
+    verbose_name = _("Qiwi via PAYONE")
+    public_name = _("Qiwi")
+    clearingtype = "wlt"
+    wallettype = "QIW"
+
+
+"""
+Test status:
+
+CC: works
+CC 3DS: works, but stays in "pending"
+eps: works, but stays in "pending"
+giropay: fails "invalid account data"
+SOFORT: works, but stays in "pending"
+SEPA DEBIT: unimplemented
+PayPal: untested (not configured)
+ideal: untested (not configured)
+przelewy24: untested (not configured)
+bancontact: untested (not configured)
+alipay: untested (not configured)
+paydirekt: untested (not configured)
+multibanco: untested (not configured)
+bct: untested (not configured)
+mybank: untested (not configured)
+paysafecard: untested (not configured)
+qiwi: untested (not configured)
+"""
