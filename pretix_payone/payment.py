32d7b5597798f89c8cc537fb24c4f130b8fd4b00
Martin Gross <gross@rami.io>
2022-05-18 13:36:29 +0200

    Fix vat_id payment parameter

--- pretix_payone/payment.py
+++ pretix_payone/payment.py
@@ -440,7 +440,7 @@ class PayoneMethod(BasePaymentProvider):
             d["country"] = guess_country(self.event) or "DE"
 
         if ia.vat_id and ia.vat_id_validated:
-            d["vatid"] = ia.vatid
+            d["vatid"] = ia.vat_id
 
         if self.invoice_address_mandatory:
             if ia.name_parts.get("salutation"):
